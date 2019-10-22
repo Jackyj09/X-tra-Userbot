4735333d77ff4e4c497faca953e58d2363f8e62f
SnapDragon <maliktahir0702@gmail.com>
2019-10-22 16:00:11 +0200

    Update xtrautil.py

--- xtrabot/xtrautil.py
+++ xtrabot/xtrautil.py
@@ -20,7 +20,7 @@ from telethon import events
 import sys
 import importlib
 from pathlib import Path
-import logger
+import logging
 
 import xtrabot.compat.userbot as userb
 import xtrabot.compat.uniborg as unib
