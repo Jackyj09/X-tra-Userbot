d03c018d7b0389d124b956e23f56b7dad3710eed
SnapDragon <maliktahir0702@gmail.com>
2019-11-01 10:36:59 +0200

    Update loader.py

--- xtrabot/loader.py
+++ xtrabot/loader.py
@@ -53,7 +53,7 @@ class Module():
         await event.reply("__Error occured on the current__ `{}`, __do__ `.log` __to show the latest log.__")
         self.logger.exception(error)
         await event.respond(traceback.format_exc())""".format(func.__name__, "."+func.__name__))
-                setattr(self, locals()[func.__name__], types.MethodType(locals()[func.__name__], self))
+                setattr(self, locals()[func.__name__].__name__, types.MethodType(locals()[func.__name__], self))
                 client.add_event_handler(locals()[func.__name__], events.NewMessage(pattern=funcmd, outgoing=True))
 
     def addxconfig(self, name, value, about=""):
