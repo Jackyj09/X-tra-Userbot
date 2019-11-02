9c7020490f650fe212f6bbe9d440914b25d2cebb
SnapDragon <maliktahir0702@gmail.com>
2019-11-02 10:42:54 +0200

    Update bwb.py

--- xtrabot/modules/bwb.py
+++ xtrabot/modules/bwb.py
@@ -17,71 +17,83 @@ wrap_users = {
 
 @loader.command(outgoing=True, pattern='!!+init')
 async def init(event):
-    await event.respond('000000init ' + bwb.init())
+    try:
+        await event.respond('000000init ' + bwb.init())
+    except:
+        pass
 
 @loader.command(outgoing=True, pattern=r"!!+add wrap (\S) ?(\d+)?")
 async def addwrap(event):
-    nick = event.pattern_match.group(1)
-    reply = await event.get_reply_message()
-    if reply == None:
-        usrid = event.pattern_match.group(2)
-    else:
-        usrid = reply.from_id
-    if nick is None or usrid is None:
-        return
-    wrap_users[nick] = int(usrid)
-    await event.respond("Added {} with user_id of {} to wrap_users".format(nick, usrid))
+    try:
+        nick = event.pattern_match.group(1)
+        reply = await event.get_reply_message()
+        if reply == None:
+            usrid = event.pattern_match.group(2)
+        else:
+            usrid = reply.from_id
+        if nick is None or usrid is None:
+            return
+        wrap_users[nick] = int(usrid)
+        await event.respond("Added {} with user_id of {} to wrap_users".format(nick, usrid))
+    except:
+        pass
 
 
 @loader.command(outgoing=True, pattern=r'!!+(e(?:enc)?)?w(?:rap)? (\S+) ([\s\S]+)')
 async def wrap(event):
-    enc = event.pattern_match.group(1) is not None
-    message = event.pattern_match.group(3)
+    try:
+        enc = event.pattern_match.group(1) is not None
+        message = event.pattern_match.group(3)
 
-    u = event.pattern_match.group(2).lower()
-    if u.isdigit():
-        u = int(u)
-    else:
-        u = wrap_users.get(u, None)
+        u = event.pattern_match.group(2).lower()
+        if u.isdigit():
+            u = int(u)
+        else:
+            u = wrap_users.get(u, None)
 
-    await event.respond(bwb.wrap(message, target=u, enc=enc), reply_to=event.reply_to_msg_id)
+        await event.respond(bwb.wrap(message, target=u, enc=enc), reply_to=event.reply_to_msg_id)
+    except:
+        pass
 
 
 @loader.command()
 async def hs(event):
-    text = bwb.parse(event.raw_text)
-    handshake_auth = False
+    try:
+        text = bwb.parse(event.raw_text)
+        handshake_auth = False
 
-    if text.startswith('000000'):
+        if text.startswith('000000'):
+            pass
+        elif bwb.check_auth(text, handshake=True):
+            handshake_auth = True
+        elif bwb.check_auth(text):
+            auth = True
+        else:
+            return
+
+        if ' ' in text:
+            command, data = text[6:].split(maxsplit=1)
+        else:
+            command, data = text[6:], None
+
+        if command == 'init' and data:
+            await event.respond('000000handshake ' + bwb.handshake(data))
+        elif command == 'handshake' and data:
+            await event.respond(bwb.wrap('secret ' + bwb.secret(data), handshake=True))
+        elif handshake_auth and command == 'secret' and data:
+            bwb.set_secret(data)
+            await event.respond(bwb.wrap('🤝'))
+        elif auth:
+            command = command.lower()
+            if command == '🤝':
+                await asyncio.sleep(1)
+                await event.respond('🤝')
+            elif command == 'ping':
+                await event.reply('Pong!')
+            elif command == "echo":
+                await do_echo(event, data)
+    except:
         pass
-    elif bwb.check_auth(text, handshake=True):
-        handshake_auth = True
-    elif bwb.check_auth(text):
-        auth = True
-    else:
-        return
-
-    if ' ' in text:
-        command, data = text[6:].split(maxsplit=1)
-    else:
-        command, data = text[6:], None
-
-    if command == 'init' and data:
-        await event.respond('000000handshake ' + bwb.handshake(data))
-    elif command == 'handshake' and data:
-        await event.respond(bwb.wrap('secret ' + bwb.secret(data), handshake=True))
-    elif handshake_auth and command == 'secret' and data:
-        bwb.set_secret(data)
-        await event.respond(bwb.wrap('🤝'))
-    elif auth:
-        command = command.lower()
-        if command == '🤝':
-            await asyncio.sleep(1)
-            await event.respond('🤝')
-        elif command == 'ping':
-            await event.reply('Pong!')
-        elif command == "echo":
-            await do_echo(event, data)
 
 async def do_echo(event, data):
         user = await event.get_sender()
