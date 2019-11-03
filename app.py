105000db1c096f2b62df9e97751b98b724d261e6
SnapDragon <maliktahir0702@gmail.com>
2019-11-03 11:45:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
+
+@app.route("/strabot/")
+def test():
+    os.system("python -m xtrabot")
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
