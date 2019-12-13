34756df57e2b48712a29952ccf1aa823030cd254
Jackyj09 <56272417+Jackyj09@users.noreply.github.com>
2019-12-13 17:16:50 +0530

    Update clk.py

--- xtrabot/modules/clk.py
+++ xtrabot/modules/clk.py
@@ -12,8 +12,8 @@ async def enaclk(event):
         person = await m.get_sender()
         user=person.first_name
         rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
-        api_token = '42c8cc27c965f4a16db748c627e1f3f39e3a0ec9'
-        req = requests.get('https://shrinknpay.com/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
+        api_token = '0889ecd7591125746f2e99994815bb5d0641543a'
+        req = requests.get('http://ptlinks.in/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
         if(req["status"] == 'error'):
           smsg = req["message"]
         else:
